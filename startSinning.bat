d1ce244bcc33c285fa2f115d3686c6bc1a1fad5f
Your Name <you@example.com>
2018-05-26 23:09:52 +0530

    first commit

---
+++ startSinning.bat
@@ -0,0 +1,5 @@
+cd /d D:\Developed\Automation\sinisterGramV2
+python "D:\Developed\Automation\python\extractMatchingLines.py" D:\Developed\Automation\inHaste\quicKlip.txt "instagram.com" >>"D:\Developed\Automation\sinisterGramV2\instalinks.opml"
+python "D:\Developed\Automation\sinisterGramV2\sinSpider.py"
+exit
+exit
